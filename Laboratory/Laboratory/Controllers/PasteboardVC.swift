50a7dcc7cd59a0ee6adc64d51df6f19f445fdc42
dongglehada <ghddns34@gmail.com>
2024-05-24 20:36:17 +0900

    파일 추가

---
+++ Laboratory/Laboratory/Controllers/PasteboardVC.swift
@@ -0,0 +1,16 @@
+//
+//  PasteboardVC.swift
+//  Laboratory
+//
+//  Created by SeoJunYoung on 5/24/24.
+//
+
+import UIKit
+
+final class PasteboardVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Laboratory/Laboratory/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Laboratory
+//
+//  Created by SeoJunYoung on 5/24/24.
+//
+
+import UIKit
+import RxCocoa
+import SnapKit
+import RxSwift
+
+final class ViewController: UIViewController {
+    
+    struct VCData {
+        var name: String
+        var vc: UIViewController
+    }
+    
+    private let controllers: BehaviorRelay<[VCData]> = .init(value: [
+        .init(name: "PasteboardTest", vc: PasteboardVC())
+    ])
+    
+    private let disposeBag = DisposeBag()
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+        setupBind()
+    }
+}
+
+
+private extension ViewController {
+    
+    func setupUI() {
+        view.backgroundColor = .red
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func setupConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setupBind() {
+        controllers.bind(to: tableView.rx.items(cellIdentifier: "Cell")) { (row, element, cell) in
+            cell.textLabel?.text = element.name
+        }
+        .disposed(by: disposeBag)
+    }
+}
