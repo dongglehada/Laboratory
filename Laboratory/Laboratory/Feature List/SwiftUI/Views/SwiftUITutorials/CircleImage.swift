45e34783ac6084dc971ec81ecc7ffbfb904555b0
dongglehada <ghddns34@gmail.com>
2024-05-28 21:37:01 +0900

    SwiftUI Tutorials UI

--- Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorials/CircleImage.swift
+++ Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorials/CircleImage.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
+        
         Image("image")
+            .resizable()
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay {
-                Circle().stroke(.white, lineWidth: 4)
-            }
+            .overlay { Circle().stroke(.white, lineWidth: 4) }
             .shadow(radius: 7)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300, height: 300)
     }
 }
 
--- Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorials/SwiftUITutorialsView.swift
+++ Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorials/SwiftUITutorialsView.swift
@@ -9,19 +9,35 @@ import SwiftUI
 
 struct SwiftUITutorialsView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            .foregroundColor(Color.green)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+
+        VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("서준영")
+                    .font(.title)
+                    .foregroundColor(Color.green)
+                HStack {
+                    Text("iOS Developer")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Busan")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                
+                Text("안녕하세요 iOS개발자 서준영입니다.")
+                    .font(.title2)
+                Text("Swift UI 학습중~!")
             }
+            .padding()
+            Spacer()
         }
-        .padding()
             
             
     }
