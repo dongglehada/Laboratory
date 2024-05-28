21b7c2945048814cd15f474a7da3b785b5ac379b
dongglehada <ghddns34@gmail.com>
2024-05-28 21:02:00 +0900

    SwiftUI Tutorials Section 3

--- Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorialsView.swift
+++ Laboratory/Laboratory/Feature List/SwiftUI/Views/SwiftUITutorialsView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct SwiftUITutorialsView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
             .foregroundColor(Color.green)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
             
             
     }
